f8aab7d85f4045f0e82446a61dbc0955bbc98076
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-11-05 12:46:44 +0100

    :CI: NEW fichier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>hello</h1>
+    <div>
+      
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>A propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque corporis non assumenda ad. Sunt, maiores. In minus minima, ipsa eos et laborum aperiam reiciendis delectus, quaerat aliquid, vero provident porro illo temporibus! Nesciunt voluptatum eaque nemo voluptates earum! Suscipit iste eveniet quasi dignissimos molestiae odio animi cum quisquam nihil voluptates, saepe quas, inventore architecto totam quos qui voluptatum aspernatur sint.</p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque corporis non assumenda ad. Sunt, maiores. In minus minima, ipsa eos et laborum aperiam reiciendis delectus, quaerat aliquid, vero provident porro illo temporibus! Nesciunt voluptatum eaque nemo voluptates earum! Suscipit iste eveniet quasi dignissimos molestiae odio animi cum quisquam nihil voluptates, saepe quas, inventore architecto totam quos qui voluptatum aspernatur sint. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque corporis non assumenda ad. Sunt, maiores. In minus minima, ipsa eos et laborum aperiam reiciendis delectus, quaerat aliquid, vero provident porro illo temporibus! Nesciunt voluptatum eaque nemo voluptates earum! Suscipit iste eveniet quasi dignissimos molestiae odio animi cum quisquam nihil voluptates, saepe quas, inventore architecto totam quos qui voluptatum aspernatur sint. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque corporis non assumenda ad. Sunt, maiores. In minus minima, ipsa eos et laborum aperiam reiciendis delectus, quaerat aliquid, vero provident porro illo temporibus! Nesciunt voluptatum eaque nemo voluptates earum! Suscipit iste eveniet quasi dignissimos molestiae odio animi cum quisquam nihil voluptates, saepe quas, inventore architecto totam quos qui voluptatum aspernatur sint.</p>
+
+        </div>
+    );
+};
+
+export default About;
