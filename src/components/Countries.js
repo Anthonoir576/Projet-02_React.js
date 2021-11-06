aa6861d7d224983e9fb232151e721fa09a8b6e0d
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-11-06 17:05:23 +0100

    :FEAT: => input radio selection

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -10,9 +10,14 @@ import Card from './Card';
 /* #########   FONCTION   ############# */
 const Countries = () => {
     
+    /***** State  *****/
     const [data, setData] = useState([]);
     const [sortedData, setSortedData] = useState([]);
     const [playOnce, setPlayOnce] = useState(true);
+    const [rangeValue, setRangeValue] = useState(40);
+    const [selectedRadio, setSelectedRadio] = useState('');
+    const radios = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+    /*****************/
 
     useEffect(() => {
 
@@ -35,24 +40,58 @@ const Countries = () => {
                 return b.population - a.population;
 
             });
+
+            sortedArray.length = rangeValue;
+            setSortedData(sortedArray);
         };
-        
         sortedCountry();
 
-    }, [data]);
+
+
+    }, [data, rangeValue, playOnce]);
     
 
     return (
         <div className="countries">
+            <div className="sort-container">
+                <input type="range" min="1" max="250" value={rangeValue} onChange={(e) => {
+
+                    setRangeValue(e.target.value);
+
+                }} />
+                <ul>
+                    {radios.map((radio) => {
+
+                        return (
+
+                            <li key={radio}>
+
+                                <input type="radio" value={ radio } id={ radio } checked={ radio === selectedRadio } onChange={ (e) => {
+
+                                    setSelectedRadio(e.target.value)
+
+                                }} />
+                                <label htmlFor={radio}>{radio}</label>
+
+                            </li>
+
+                        );
+
+                    })}
+                </ul>
+            </div>
             <ul className="countries-list">
-                { data.map((country) => (
+                { sortedData
+                    .filter((country) => country.region.includes(selectedRadio))
+                    .map((country) => (
 
                     <Card country={country} key={country.name.official} />
+
                 ))}
             </ul>
         </div>
     );
-    
+
 };
 /* #################################### */
 
