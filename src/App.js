aaf4f4ac8ea1fb404351874a35201906968967d1
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-11-06 10:16:45 +0100

    :FEAT: Config path routage app.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
+
+/* ####### IMPORT / DECLARATION ####### */
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import About from './pages/About';
+/* #################################### */
 
 
+
+/* #########   FONCTION   ############# */
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/a-propos" exact component={About} />
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
 };
+/* #################################### */
 
+
+/* ###########  EXPORT  ############### */
 export default App;
+/* #################################### */
